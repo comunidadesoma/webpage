063e71161f320776b60c551c555170563c7189e6
Vinicius Pinheiro <vinicius.pinheiro88@hotmail.com.br>
2022-06-12 02:49:38 -0300

    Primeiros ajustes responsividade

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 //Declarando Variáveis
 var btnContact = document.querySelector('.is-btn-contact');
+var toggleMenu = document.querySelectorAll('.is-toggle-menu');
+var menuMobile = document.querySelector('.is-menu-mob');
+var btnMenuMobIcon = document.querySelector('.is-btn-menu-mob ion-icon');
+
 
 //Page Preloader
 window.addEventListener('load', function () {
@@ -17,6 +21,24 @@ btnContact.addEventListener('click', function(){
     boxContact.classList.toggle('is-is-open');
     this.classList.toggle('is-change-icon');
 });
+
+//Abrindo e Fechando o Menu Mobile
+for (var m = 0; m < toggleMenu.length; m++) {
+        toggleMenu[m].addEventListener('click', function () {
+            var overlay = document.querySelector('.is-menu-overlay');
+            overlay.classList.toggle('is-is-open')
+            menuMobile.classList.toggle('is-menu-is-closed');
+            menuMobile.classList.toggle('is-menu-is-open');
+
+            var icon = btnMenuMobIcon.getAttribute('name');
+
+            if(icon === 'menu-outline'){
+                btnMenuMobIcon.setAttribute('name', 'close');
+            } else{
+                btnMenuMobIcon.setAttribute('name', 'menu-outline');
+            }
+    });
+}
 
 //Animando Elementos on Scrool com Waypoints
 var myScrollDown = document.querySelector('.is-scroll-down');
